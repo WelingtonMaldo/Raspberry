4ab3ebb8b4aec6d903aa255b5e1792e152377ed4
WelingtonMaldo <welington.maldo@gmail.com>
2021-10-07 16:34:45 +0200

    Raspberry

--- Pruebas2.py
+++ Pruebas2.py
@@ -7,7 +7,7 @@ from time import sleep
 #VACIAR LA TERMINAL
 
 #system('cls')                  Este comando sirve en Windows 10
-system('clear')                 #Este comando sirve en Linux 
+system('clear')                 #Este comando sirve en Linux y Mac
 
 #MENSAJES INICIALES
 

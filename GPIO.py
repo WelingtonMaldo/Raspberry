9c3679f4fa6fac9651c8543cb5e65b9fda7caac0
WelingtonMaldo <welington.maldo@gmail.com>
2021-10-08 15:20:51 +0200

    Prueba con el LED

--- GPIO.py
+++ GPIO.py
@@ -5,8 +5,51 @@
 from RPi import GPIO
 #import RPi.GPIO as GPIO
 import time
+import sys, select
+from os import system
+
+system('clear')
 
 #Se configura los pines de la raspberry
 GPIO.setmode(GPIO.BOARD)                           #Esto es para poder llamar a los Pines conforme estan numerado "fisicamente" en la raspberry 
-GPIO.setup(7, GPIO.out)
+GPIO.setup(7, GPIO.OUT)
+Contador = 0
+Resto = 0
+Continuar = True
+Temporizador = 3
+
+while Continuar == True:
+    Contador = Contador+1
+    Resto = Contador%5
+    #print('Contador = ',Contador,'\n')
+
+    GPIO.output(7,True)
+    time.sleep(0.2)
+    GPIO.output(7,False)
+    
+    
+    time.sleep(0.25)
+    if Resto ==  0:
+        i = 0
+        print('Temporizador: %d segundos para responder'%Temporizador)
+        print("¿Se desea parar la ejecución? (Y/N)\n")
+        
+        i, o, e = select.select( [sys.stdin], [], [], Temporizador)
+
+        if (i):
+            Respuesta = sys.stdin.readline().strip()
+            print('\nLa respuesta es...',Respuesta )
+        else:
+            Respuesta = 'N'
+            print("No se obtuvo respuesta del usuario\n")
+
+        if Respuesta.upper() == 'Y':
+            print("Se finaliza la ejecución\n")
+            Continuar=False
+           # sys.exit()
+    
+
+
+
+
 
---
+++ Prueba_V0.py
@@ -0,0 +1,6 @@
+Name = input('¿A quien se quiere saludar?')
+#class tkinter.messagebox.Message(master=None,**options)
+#tkinter.messagebox.showinfo(title=None, message=None, **options)
+print("Hello "+Name+" :D")
+import tkMessageBox
+tkMessageBox.showinfo(title="Welington", message="Hola "+Name+" :D")
